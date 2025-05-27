5da1df770594ee587d93cb15ab137c7e2c0984de
Lokeswara Reddy <166979268+techabbayi@users.noreply.github.com>
2025-05-27 22:16:35 +0530

    Update axiosConfig.js

--- frontend/api/axiosConfig.js
+++ frontend/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Set default base URL for all requests
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000' || 'https://s86-worst-passwords-everused.onrender.com',
 });
 
 export default axiosInstance;
