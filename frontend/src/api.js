aab857c327e0f042e7ff01d2b5d5ee82a25ae1b1
Lokeswara Reddy <166979268+techabbayi@users.noreply.github.com>
2025-05-27 22:15:35 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://s86-worst-passwords-everused.onrender.com',
   withCredentials: true
 });
 
