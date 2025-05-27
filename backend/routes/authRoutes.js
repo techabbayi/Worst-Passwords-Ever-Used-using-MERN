475e9f0f86283e44db5f4e242c4e42595667f167
Lokeswara Reddy <166979268+techabbayi@users.noreply.github.com>
2025-05-27 21:43:08 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 const authenticate = require('../middleware/authenticate');
 
